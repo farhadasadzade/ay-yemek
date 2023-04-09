b9b9ffccb9415dd99e7a4fffa54656af4a700fe7
Farhad Asadzade <f.esedzade9789@gmail.com>
2023-04-09 19:42:05 +0400

    fix

--- src/pages/Profile/UserInfo.jsx
+++ src/pages/Profile/UserInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import i18n from "i18next";
-import { useMount, useUnmount } from "ahooks";
+import { useHistory } from "react-router-dom";
+import { useMount, useUnmount, useMemoizedFn } from "ahooks";
 import { lowerCase } from "lodash";
 import { Col, Row, Form } from "antd";
 import { api } from "common/api/api";
@@ -8,14 +9,67 @@ import { Button, Input } from "common/components";
 import { plusOrange } from "assets/icons";
 import { Link } from "react-router-dom";
 
+const monthNames = {
+  az: [
+    "Yanvar",
+    "Fevral",
+    "Mart",
+    "Aprel",
+    "May",
+    "İyun",
+    "İyul",
+    "Avqust",
+    "Sentyabr",
+    "Oktyabr",
+    "Noyabr",
+    "Dekabr",
+  ],
+  en: [
+    [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ],
+  ],
+  ru: [
+    "январь",
+    "февраль",
+    "март",
+    "апрель",
+    "май",
+    "июнь",
+    "июль",
+    "август",
+    "сентябрь",
+    "октябрь",
+    "ноябрь",
+    "декабрь",
+  ],
+};
+
 const UserInfo = () => {
   const { t } = i18n;
   const language = lowerCase(localStorage.getItem("lang"));
   const userToken = localStorage.getItem("userToken");
+  const history = useHistory();
 
   const [windowWidth, setWindowWidth] = React.useState(0);
 
   const [getUserData, userDataState] = api.useLazyGetUserDataQuery();
+
+  const handleUpdatePackage = useMemoizedFn((id) => {
+    localStorage.setItem("selectedPackageId", id);
+    history.push("/payment");
+  });
 
   useMount(() => {
     window.addEventListener("resize", (e) =>
@@ -32,7 +86,7 @@ const UserInfo = () => {
   });
 
   useUnmount(() => {
-    window.removeEventListener("resize", () => {});
+    window.removeEventListener("resize", () => { });
   });
 
   return (
@@ -40,13 +94,24 @@ const UserInfo = () => {
       <Row>
         <h1 className="profile__main-title">{t("choosenCategory")}</h1>
       </Row>
-      <Row className="mb-3" align="middle" justify="space-between" wrap>
-        <h2 className="profile__main-name">
-          Ekonom (10 gunluk){" "}
-          <span>etibarlidir 17 Yanvar 2023 - 27 Yanvar 2023</span>
-        </h2>
-        <Button type="secondary">{t("updatePackage")}</Button>
-      </Row>
+      {userDataState.data?.data?.packageOrders?.map(order => {
+
+        const startedDate = order?.started_date?.split('-')
+        const endedDate = order?.ended_date?.split('-')
+
+        const startedMonth = startedDate?.[1]?.[0] === '0' ? startedDate?.[1]?.[1] : startedDate?.[1]
+        const endedMonth = endedDate?.[1]?.[0] === '0' ? endedDate?.[1]?.[1] : endedDate?.[1]
+
+        return (
+          <Row className="mb-3" align="middle" justify="space-between" wrap>
+            <h2 className="profile__main-name">
+              {order?.package?.category?.name} ({order?.package?.name}){" "}
+              <span>etibarlidir {startedDate?.[2]} {monthNames[language]?.[startedMonth - 1]} {startedDate?.[0]} - {endedDate?.[2]} {monthNames[language]?.[endedMonth - 1]} {endedDate?.[0]}</span>
+            </h2>
+            <Button onClick={() => handleUpdatePackage(order?.package?.id)} type="secondary">{t("updatePackage")}</Button>
+          </Row>
+        )
+      })}
       <Link to="/home/categories">
         <button className="profile__main-addPacket">
           <Row align="middle">
